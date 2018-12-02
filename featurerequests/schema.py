414193102272c526fcb81bcdafb24414b591228b
Eyitemi Egbejule <insaida@Eyitemis-MacBook-Pro.local>
2018-12-02 17:23:33 +0100

    + schema, modified views

---
+++ featurerequests/schema.py
@@ -0,0 +1,24 @@
+from marshmallow import Schema, fields, validate
+
+
+class FeaturesRequestSchema(Schema):
+    id = fields.Int(dump_only=True)
+    title = fields.Str(
+        required=True,
+        validate=[validate.Length(min=6, max=255)]
+    )
+    description = fields.Str(required=True)
+    client_priority = fields.Int(
+        required=True,
+        validate=[validate.Range(min=1)]
+    )
+    client_id = fields.Int(
+        required=True,
+        load_from="client"
+    )
+    product_area_id = fields.Int(
+        required=True,
+        load_from="product_area"
+    )
+
+    
--- featurerequests/views.py
+++ featurerequests/views.py
@@ -2,7 +2,15 @@ from flask import jsonify, render_template, request
 
 from featurerequests import app
 
+from featurerequests.models import db, FeaturesRequest, Client, ProductArea
+
 
 @app.route("/")
 def homepage():
-    return render_template('base.html')
+    return render_template('index.html')
+
+
+@app.route("/api/feature_requests/", methods=("GET"))
+def feature_requests_get_method():
+    feature_requests = FeaturesRequest.query.all()
+    
